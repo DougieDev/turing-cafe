b033eb337116439ac22c085f37b483c3d40cfa79
Carlyle Douglas <cdouglascorp@gmail.com>
2020-10-21 11:29:12 -0600

    Render form component and give it ability to make reservations

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { getReservations } from '../apiCalls';
 import CardContainer from '../CardContainer/CardContainer'
+import Form from '../Form/Form'
 
 
 class App extends Component {
@@ -19,11 +20,17 @@ class App extends Component {
     .catch(error => this.setState({ error: "Something is amiss!" }))
   }
 
+  createReservation = (reservation) => {
+    this.setState({ reservations: [...this.state.reservations], reservation})
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'></div>
+        <div className='resy-form'>
+          <Form createReservation={this.createReservation} />
+        </div>
         <div className='resy-container'>
           <CardContainer reservations={this.state.reservations} />
         </div>
