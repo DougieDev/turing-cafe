e9119455aefa38ca092ccf7d90b8d039782267c5
Carlyle Douglas <cdouglascorp@gmail.com>
2020-10-21 10:42:25 -0600

    Add func components for CardContainer

---
+++ src/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CardContainer.css'
+import Card from '../Card/Card'
+
+const CardContainer = ({ reservations }) => {
+    const cards = reservations.map((reservation) => {
+        return <Card { ...reservation } />
+    })
+    return <section className='card-container'>{cards}</section>
+
+};
+
+export default CardContainer;
